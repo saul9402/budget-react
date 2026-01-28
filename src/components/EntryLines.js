42c57d652e593c76dbb74edf40cd6ad49923d884
Saul Oliver <saul.avila.dev@outlook.com>
2026-01-27 18:48:42 -0600

    refactor: manage entries state and dynamic actions in App

---
+++ src/components/EntryLines.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import EntryLine from "./EntryLine";
+
+const EntryLines = ({entries, deleteEntry}) => {
+    return (
+        <>
+            {entries.map((entry) => (
+                <EntryLine key={entry.id} {...entry} deleteEntry={deleteEntry}/>
+            ))}
+        </>
+    );
+};
+
+export default EntryLines;
